bd4832f4552b95bc68632776fc648d2a97130958
sunny2831 <sunny2831@hotmail.co.uk>
2018-10-15 16:26:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var beatlesPlay = []
+  for (var i = 0; i < musicians.length; i++) {
+    beatlesPlay.push (`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return beatlesPlay
+}
+
+function johnLennonFacts(facts) {
+  let i = 0
+while (i < facts.length) {
+  facts[i] += "!!!"
+  i++
+    }
+}
