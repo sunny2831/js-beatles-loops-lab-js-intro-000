a0079cbf15dab17d6c803776a0c80571c9c4e3a0
sunny2831 <sunny2831@hotmail.co.uk>
2018-10-15 14:44:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-function theBeatlesPlay(musicians, instruments) {
+var musicians = []
+var instruments = []
 
+function theBeatlesPlay(musicians, instruments) {
+  for (var musicians = [0]) {
+  console.log("John Lennon plays guitar");
+}
 }
